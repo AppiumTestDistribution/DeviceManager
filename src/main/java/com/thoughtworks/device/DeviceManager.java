dde17ea5749282d2eb3c44f16bbeb43fdf159b3c
saikrishna321 <saikrishna321@yahoo.com>
2017-07-08 15:18:25 +0530

    #4 get UDID from deviceName and OS version

--- src/main/java/com/thoughtworks/device/DeviceManager.java
+++ src/main/java/com/thoughtworks/device/DeviceManager.java
@@ -4,7 +4,9 @@ package com.thoughtworks.device;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thoughtworks.utils.CommandPromptUtil;
+
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -25,13 +27,32 @@ public class DeviceManager {
                 runCommandThruProcess("xcrun simctl list -j devices");
         ObjectMapper mapper = new ObjectMapper();
         Map<String, Object> map;
-        map = mapper.readValue(fetchSimulators, new TypeReference<Map<String, Object>>() {});
+        map = mapper.readValue(fetchSimulators, new TypeReference<Map<String, Object>>() {
+        });
         Object devices = map.get("devices");
         ((LinkedHashMap) devices).forEach((key, value) -> {
-            if(((String) key).split(" ")[0].equals("iOS")) {
-                availableSimulators.put(key,value);
+            if (((String) key).split(" ")[0].equals("iOS")) {
+                availableSimulators.put(key, value);
             }
         });
         return availableSimulators;
     }
+
+    public String getSimulatorUDID(String deviceName, String OS)
+            throws Throwable {
+        HashMap<Object, Object> allSimulators = getAllSimulators();
+        Object filerOS = allSimulators.entrySet().stream().filter(objectObjectEntry ->
+                objectObjectEntry.getKey().toString().contains(OS))
+                .findFirst().map(objectObjectEntry -> {
+                    return objectObjectEntry.getValue();
+                }).orElseThrow(() -> new RuntimeException("Incorrect OS version is provided -- " + OS));
+
+        Object getUDID = ((ArrayList) filerOS).stream().filter(o ->
+                ((LinkedHashMap) o).get("name").
+                        equals(deviceName)).findFirst().map(o -> {
+            return ((LinkedHashMap) o).get("udid");
+        }).orElseThrow(() -> new RuntimeException("Incorrect DeviceName is provided -- " + deviceName));
+        return (String) getUDID;
+    }
+
 }
--- src/test/java/com/thoughtworks/device/SimulatorTest.java
+++ src/test/java/com/thoughtworks/device/SimulatorTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.io.IOException;
 import java.util.HashMap;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class SimulatorTest {
@@ -16,5 +17,35 @@ public class SimulatorTest {
         deviceManager = new DeviceManager();
         HashMap<Object, Object> allSimulators = deviceManager.getAllSimulators();
         assertTrue(allSimulators.size() > 0);
+    }
+
+    @Test
+    public void getSimulatorUDID() throws Throwable {
+        deviceManager = new DeviceManager();
+        String simulatorUDID = deviceManager.getSimulatorUDID
+                ("iPhone 6s", "11.0");
+        assertTrue(simulatorUDID.length() == 36);
+    }
+
+    @Test
+    public void throwExceptionWhenInvalidOSVersionIsGiven() throws Throwable {
+        deviceManager = new DeviceManager();
+        try {
+            deviceManager.getSimulatorUDID
+                    ("iPhone 6s", "9.99");
+        } catch (RuntimeException e) {
+            assertEquals(e.getMessage(),"Incorrect OS version is provided -- 9.99");
+        }
+    }
+
+    @Test
+    public void throwExceptionWhenInvalidDeviceNameVersionIsGiven() throws Throwable {
+        deviceManager = new DeviceManager();
+        try {
+            deviceManager.getSimulatorUDID
+                    ("iPhone 6ss", "11.0");
+        } catch (RuntimeException e) {
+            assertEquals(e.getMessage(),"Incorrect DeviceName is provided -- iPhone 6ss");
+        }
     }
 }
