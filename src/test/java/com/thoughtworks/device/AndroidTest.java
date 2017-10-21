cac4fb08b856d501b93d87ff14f4891c2cfe1ca2
Srinivasan Sekar <srinivasan.sekar1990@gmail.com>
2017-10-21 14:11:01 +0530

    add installapp test

--- src/test/java/com/thoughtworks/device/AndroidTest.java
+++ src/test/java/com/thoughtworks/device/AndroidTest.java
@@ -4,6 +4,8 @@ package com.thoughtworks.device;
 import com.thoughtworks.android.AndroidManager;
 import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
+
 public class AndroidTest {
 
     AndroidManager deviceManager;
@@ -13,7 +15,13 @@ public class AndroidTest {
     public void getDetails() throws Exception {
         deviceManager = new AndroidManager();
         Device deviceProperties = deviceManager.getDeviceProperties("192.168.58.101:5555");
-        System.out.println(deviceProperties.getIsDevice());
+        System.out.println(deviceProperties..getIsDevice());
         System.out.println(deviceProperties.getScreenSize());
+    }
+
+    @Test
+    public void installAppTest() throws Exception {
+        deviceManager = new AndroidManager();
+        deviceManager.installApp("/Users/ssekar/AndroidCalculator.apk");
     }
 }
